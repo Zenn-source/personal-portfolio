8bb735e1566eca549ba86c114454b5c32f8efa00
Mackenzie Iguiron <mackenzie.g.iguiron@gmail.com>
2026-06-03 17:50:49 +0800

    Add About component to Portfolio layout

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -4,6 +4,7 @@ import Ticker from "./components/Ticker";
 import Work from "./components/Work";
 import Services from "./components/Services";
 import Stack from "./components/Stack";
+import About from "./components/About";
 import Philosophy from "./components/Philosophy";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
@@ -19,6 +20,7 @@ export default function Portfolio() {
         <Experience />
         <Work />
         <Stack />
+        <About />
         <Services />
         <Philosophy />
         <Ticker items={LIFE_TICKER} sep="✦" />
